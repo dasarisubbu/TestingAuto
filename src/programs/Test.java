f583eaa43ffca52fe56297cf1b45a268ae2c14bb
SUBBU <SUBBU@DESKTOP-B90T5T6>
2021-08-03 13:57:05 +0530

    Commiting 2nd Script

---
+++ src/programs/Test.java
@@ -0,0 +1,13 @@
+package programs;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   int i=7;
+   int j=7;
+   int v=i+j;
+   System.out.println(v);
+	}
+
+}
